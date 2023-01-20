65b40dcb3398bd66a96da59a199c07b8ed22cff9
christrilogy <112494599+christrilogy@users.noreply.github.com>
2023-01-20 14:27:39 -0500

    Lab1-code
    .

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout<<"----------------------"<<endl;
+  cout<<"Christian Morgan"<<endl;
+  cout<<"Computer Science 1"<<endl;
+  cout << "Orange County Community College" << endl;
+  cout<<"lab1"<< endl;
+  cout<<"January 20, 2023"<<endl;
+  cout<<"----------------------"<<endl;
+  int x; // integer variable
+  int y; // integer variable  
+  x = 20;
+  //cout<<"content of X right after initialization: " << x << endl;
+  y = 5;
+  //cout<<"X + Y = " << x + y<< endl;
+
+  return 0;
+}
