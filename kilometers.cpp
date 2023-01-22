c1e2523c0f47f97ab1f135a8770a9f8e18856626
christrilogy <112494599+christrilogy@users.noreply.github.com>
2023-01-22 18:22:55 -0500

    Create kilometers.cpp

---
+++ kilometers.cpp
@@ -0,0 +1,22 @@
+
+// Christian Morgan
+
+#include <iostream>
+using namespace std;
+
+int main ()
+
+{
+	int number;
+	float total;
+	
+	cout << "Converting kilometers to miles" << endl;
+	cout << "Enter the amount of kilometers" << endl;
+	cin >> number;
+	
+	total = number * 0.621;
+	cout << total << " the lenght in miles" << endl;
+	
+	return 0;
+	
+	}
