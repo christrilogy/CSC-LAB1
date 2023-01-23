fb4f9bf7d654494b9395f00dab96b877f2357861
christrilogy <112494599+christrilogy@users.noreply.github.com>
2023-01-23 16:45:40 -0500

    Update firstprog.cpp

--- firstprog.cpp
+++ firstprog.cpp
@@ -7,6 +7,16 @@ using namespace std;
 int main() 
 
 {
+	cout<<"----------------------"<<endl;
+  	cout<<"Christian Morgan"<<endl;
+  	cout<<"Computer Science 1"<<endl;
+  	cout << "Orange County Community College" << endl;
+  	cout<<"lab1"<< endl;
+  	cout<<"January 20, 2023"<<endl;
+  	cout<<"----------------------"<<endl;
+	
+	
+	
 	cout << "Now is the time for all good men" << endl;
 	cout << "To come to the aid of their party" << endl;
  	 
